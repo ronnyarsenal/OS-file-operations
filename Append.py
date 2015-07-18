6325e01c674d3e6852d8c6ae59d7c5c198dee058
Rondal Archer <ronnyarsenal@yahoo.com>
2015-07-18 16:17:39 -0500

    Create Append.py

---
+++ Append.py
@@ -0,0 +1,7 @@
+__author__ = 'ron'
+def appendFile(): //function to append
+ Name = input("what is the name of the file?")
+ file_object = open(Name,"a") 
+ Text = input("Insert the text you would like to append")
+ file_object.write("\n" + Text + " ")
+ file_object.close()
